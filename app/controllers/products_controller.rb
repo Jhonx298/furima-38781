4938407820675d28d83f6788921ec4a156c57d39
ジュン <108872537+Jhonx298@users.noreply.github.com>
2023-03-16 22:09:03 +0900

    商品出品機能(MVC)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,27 @@
 class ProductsController < ApplicationController
-  def index 
+  before_action :authenticate_user!, except: :index
 
+  def index
+    @products = Product.order("created_at DESC")
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :product_description, :price, :image, :category_id,
+      :condition_id, :shipping_charge_id, :prefecture_id, :days_ship_id).merge(user_id:current_user.id)
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,22 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one    :purchase_record
   has_one_attached :image
+  
 
   validates :name,                presence: true
   validates :product_description, presence: true
   validates :price,               presence: true
+
+  validates :category_id,        numericality: { other_than: 1, message: "can`t be blank" }
+  validates :condition_id,       numericality: { other_than: 1, message: "can`t be blank" }
+  validates :shipping_charge_id, numericality: { other_than: 1, message: "can`t be blank" }
+  validates :prefecture_id,      numericality: { other_than: 1, message: "can`t be blank" }
+  validates :days_ship_id,       numericality: { other_than: 1, message: "can`t be blank" }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :days_ship
 end
