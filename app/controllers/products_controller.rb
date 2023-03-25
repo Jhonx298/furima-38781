9c5dfeb4488731681eba43c3ad2ac37207f27805
ジュン <108872537+Jhonx298@users.noreply.github.com>
2023-03-25 16:32:53 +0900

    Revert "product#edit(MVC)"
    This reverts commit 1f06d29c12e6b70bbfaa7f26aab0c7efca55275b.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,19 +22,6 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
-
-  def update
-    @product = Product.find(params[:id])
-    if @product.update(product_params)
-      redirect_to product_path
-    else
-      render 'products/edit'
-    end
-  end
-
   private
   def product_params
     params.require(:product).permit(:name, :product_description, :price, :image, :category_id,
