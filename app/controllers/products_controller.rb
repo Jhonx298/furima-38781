cb48bc8f4406d9ed82424df46ebe78ace48b247e
ジュン <108872537+Jhonx298@users.noreply.github.com>
2023-03-25 17:59:37 +0900

    product#controller(private:set_product)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :set_product, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :correct_user, only: [:edit, :update]
 
@@ -20,15 +21,12 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path
     else
@@ -40,6 +38,10 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:name, :product_description, :price, :image, :category_id,
       :condition_id, :shipping_charge_id, :prefecture_id, :days_ship_id).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 
   def correct_user
